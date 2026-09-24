ff0cff9a5918683721c84e87da7bb7ff9465f8d8
Jonathan Steele <iNoles@users.noreply.github.com>
2026-09-24 17:08:23 -0400

    Refactor BrowserRouter to use basename

--- my-rides-app/src/main.tsx
+++ my-rides-app/src/main.tsx
@@ -1,15 +1,18 @@
-import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css"; // dark mode styles here
+import "./index.css";
+
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </HelmetProvider>
